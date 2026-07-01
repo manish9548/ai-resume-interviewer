2db51d456c50c9709d8a30cc50bfe89f8d87999c
manish9548 <baghelmanish4947@gmail.com>
2026-07-01 10:34:11 +0530

    Initial commit - AI Resume Interviewer

---
+++ ai-resume-interviewer/src/main/java/com/manish/airesumeinterviewer/AiResumeInterviewerApplication.java
@@ -0,0 +1,13 @@
+package com.manish.airesumeinterviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResumeInterviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResumeInterviewerApplication.class, args);
+	}
+
+}
---
+++ ai-resume-interviewer/src/test/java/com/manish/airesumeinterviewer/AiResumeInterviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.airesumeinterviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiResumeInterviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
