7e849668c1037fbc1c3256ef21dee87eb500130f
manish9548 <baghelmanish4947@gmail.com>
2026-07-02 08:30:47 +0530

    authentication api created abd tested successfully

--- ai-resume-interviewer/src/main/java/com/manish/airesumeinterviewer/controller/AuthController.java
+++ ai-resume-interviewer/src/main/java/com/manish/airesumeinterviewer/controller/AuthController.java
@@ -1,5 +1,24 @@
 package com.manish.airesumeinterviewer.controller;
 
+import com.manish.airesumeinterviewer.dto.RegisterRequest;
+import com.manish.airesumeinterviewer.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthService service;
+    @PostMapping("/register")
+    public String register(@Valid @RequestBody RegisterRequest request){
+        return service.register(request);
+
+    }
 
 }
