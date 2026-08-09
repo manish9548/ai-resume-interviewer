a0a9d34e15cc0da6c98f0a9972c46ca1f71c2bd6
manish9548 <baghelmanish4947@gmail.com>
2026-08-09 10:34:15 +0530

    fix

--- frontend/src/services/resumeService.js
+++ frontend/src/services/resumeService.js
@@ -1,36 +1,21 @@
 import api from "../utils/axiosConfig";
 
 export const uploadResume = async (file) => {
-
     const formData = new FormData();
-
     formData.append("file", file);
 
-    const response = await api.post(
-        "/resume/upload",
-        formData,
-        {
-            headers: {
-                "Content-Type": "multipart/form-data",
-            },
-        }
-    );
+    // Yahan headers object hata do, Axios multipart/form-data aur boundary khud handle kar lega
+    const response = await api.post("/resume/upload", formData);
 
     return response.data;
 };
 
 export const getResumeHistory = async () => {
-
     const response = await api.get("/resume/history");
-
     return response.data;
-
 };
 
 export const analyzeResume = async () => {
-
     const response = await api.get("/resume/analyze");
-
     return response.data;
-
 };
