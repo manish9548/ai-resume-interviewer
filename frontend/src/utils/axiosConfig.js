a7b1615ab55d04b3d4e8e333ef3139a42c516cdb
manish9548 <baghelmanish4947@gmail.com>
2026-08-09 07:48:12 +0530

    axiosfix

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,16 +1,17 @@
 import axios from "axios";
-const api=axios.create({
-    baseURL : "http://localhost:8080/api",
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
     headers: {
-         "Content-Type": "application/json",
+        "Content-Type": "application/json",
     },
 });
 
-api.interceptors.request.use((config)=>{
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
 
-    if(token){
-        config.headers.Authorization =  `Bearer ${token}`;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 });
