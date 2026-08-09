90f5a7a7d92091aa5484bb8848f40bc5fdb2fc2c
manish9548 <baghelmanish4947@gmail.com>
2026-08-09 08:32:34 +0530

    changed the cors

--- backend/src/main/java/com/manish/airesumeinterviewer/config/CorsConfig.java
+++ backend/src/main/java/com/manish/airesumeinterviewer/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
                 List.of(
                         "http://localhost:5173",
                         "http://localhost:5174",
-                        "https://ai-resume-interviewer-git-main-manish9548s-projects.vercel.app"
+                        "https://ai-resume-interviewer.vercel.app"
                 )
         );
 
